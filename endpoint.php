7003f198056cc71a4b92fc7558670549241b2541
quest1onmark <jonas.schultheiss3@gmail.com>
2018-11-09 00:10:24 +0100

    update

--- endpoint.php
+++ endpoint.php
@@ -5,3 +5,21 @@
  * Date: 08.11.18
  * Time: 15:07
  */
+
+// error_reporting(0);
+include('request_handler.php');
+print_r(via_to_array($_POST['via']));
+echo "<br>";
+echo transportation_types_formatter($_POST['train'], $_POST['tram'], $_POST['bus'], $_POST['ship'], $_POST['cableway']);
+echo "<br>";
+print_r(GET(
+        parse_data_to_url(
+            $_POST['from'],
+            $_POST['to'],
+            via_to_array($_POST['via']),
+            $_POST['date_time_info'],
+            $_POST['date_time'],
+            transportation_types_formatter($_POST['train'], $_POST['tram'], $_POST['bus'], $_POST['ship'], $_POST['cableway'])
+        )
+    )
+);
--- request_handler.php
+++ request_handler.php
@@ -7,13 +7,50 @@
  */
 
 
-$from = urlencode("");
-$to = urlencode("");
+// $url = "https://fahrplan.search.ch/api/route.json?from=Therwil&to=Ettingen&num=&transportation_types=&date=&time=";
 
-$url = "https://fahrplan.search.ch/api/route.json?from=Therwil&to=Ettingen&num=&transportation_types=&date=&time=";
 
-$body = json_decode(file_get_contents($url), true);
+// $body = json_decode(file_get_contents($url), true);
 
-foreach ($body['connections'] as $key => $value) {
+function parse_data_to_url($from, $to, $via, $date_time_info = "", $date_time = "", $transportation_types = "")
+{
+    static $base_url = "https://fahrplan.search.ch/api/route.json?";
+    $base_url .= "from='" . urlencode($from) . "\'&";
+    $base_url .= "to='" . urlencode($to) . "\'&";
+    if ($via !== "") {
+        foreach ($via as $key => $value) {
 
+        }
+    }
 }
+
+function via_to_array($via = "")
+{
+    if ($via === "") {
+        return "";
+    } else {
+        return explode(",", $via);
+    }
+}
+
+function transportation_types_formatter($train = "", $tram = "", $bus = "", $ship = "", $cableway = "")
+{
+    static $final_string = "";
+    $final_string .= $train !== "" ? $train . "," : "";
+    $final_string .= $tram !== "" ? $tram . "," : "";
+    $final_string .= $bus !== "" ? $bus . "," : "";
+    $final_string .= $ship !== "" ? $ship . "," : "";
+    $final_string .= $cableway !== "" ? $cableway . "," : "";
+
+    return $final_string;
+}
+
+
+function GET($url)
+{
+    return json_decode(file_get_contents($url), true);
+}
+
+//foreach ($body['connections'] as $key => $value) {
+//
+//}
